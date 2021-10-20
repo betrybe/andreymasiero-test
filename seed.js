f9e1b37d8f157751a4bd8094f22429f56f62ddbd
Andrey Masiero <andreymasiero@gmail.com>
2021-10-20 11:51:01 -0300

    Create seed file to populate db with admin user.

--- seed.js
+++ seed.js
@@ -1,1 +1,20 @@
-// colocar query do MongoDB
+const { MongoClient } = require('mongodb');
+const database = require('./src/api/config/database');
+const User = require('./src/api/models/User');
+
+const client = new MongoClient(database.url, { useUnifiedTopology: true });
+
+async function execute() {
+	try {
+		const user = new User('admin', 'root@email.com', 'admin', null, 'admin');
+		await client.connect();
+		const db = client.db(database.name);
+		const collection = db.collection('users');
+		const result = await collection.insertOne(user);
+		console.log(`A user was created with _id: ${result.insertedId}`);
+	} finally {
+		await client.close();
+	}
+}
+
+execute().catch(console.dir);
